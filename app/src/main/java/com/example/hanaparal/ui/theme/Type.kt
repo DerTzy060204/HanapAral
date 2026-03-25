2bcb9f28d20d515e081aa4824c3196e724d91972
Cassandra Sante <cassandrasante18@gmail.com>
2026-03-25 19:52:33 +0800

    Sante- style: Define standardized typography for academic readability

--- app/src/main/java/com/example/hanaparal/ui/theme/Type.kt
+++ app/src/main/java/com/example/hanaparal/ui/theme/Type.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     displayLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -67,5 +66,4 @@ val Typography = Typography(
         fontSize = 12.sp,
         lineHeight = 16.sp
     )
-    */
 )
